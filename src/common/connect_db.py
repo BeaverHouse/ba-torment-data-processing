f8e16192aba0a86dcf02ca71731dc7b5e0d9fb65
Austin Lee <haulrest@gmail.com>
2025-03-31 01:26:01 +0900

    Enable autocommit on initializing DB connection
    어차피 DB 업데이트 시 commit을 할 것이므로 여기서 미리 autocommit을 활성화 합니다.

--- src/common/connect_db.py
+++ src/common/connect_db.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_postgres() -> psycopg2.extensions.connection:
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD"),
         host=os.getenv("POSTGRES_HOST"),
         port=os.getenv("POSTGRES_PORT"),
         database=os.getenv("POSTGRES_DBNAME"),
     )
+    conn.autocommit = True
+    return conn
 
 def get_postgres_client() -> psycopg2.extensions.cursor:
     return get_postgres().cursor()
